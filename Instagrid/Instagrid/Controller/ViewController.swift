7e4ebb2132ea94b65d81a9007d5c581fd9d44412
queency972 <steve.bernard972@gmail.com>
2019-11-02 11:36:20 -0500

    Connection de: SwipeButton (@IBOutlet) 4 buttonImport (@IBOutlet) 3 viewLayout (@IBOutlet)
    Connection de:
    3 buttonLayout (@IBAction)
    swipeActionV2 (@IBAction)

--- Instagrid/Instagrid/Controller/ViewController.swift
+++ Instagrid/Instagrid/Controller/ViewController.swift
@@ -11,15 +11,64 @@ import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
-    @IBOutlet weak var btn3: UIButton!
 
-    @IBAction func buttonImport(_ sender: Any) {
+    @IBOutlet weak var swipeView: UIButton!
+    @IBOutlet weak var buttonImport1: UIButton!
+    @IBOutlet weak var buttonImport2: UIButton!
+    @IBOutlet weak var buttonImport3: UIButton!
+    @IBOutlet weak var buttonImport4: UIButton!
+    @IBOutlet weak var viewLayout1: UIButton!
+    @IBOutlet weak var viewLayout2: UIButton!
+    @IBOutlet weak var viewLayout3: UIButton!
+
+
+    @IBAction func layout1(_ sender: Any) {
+        buttonImport1.isHidden = false
+        buttonImport2.isHidden = true
+        buttonImport3.isHidden = false
+        buttonImport4.isHidden = false
+        viewLayout1.setImage(UIImage(named: "Selected"), for: .normal)
+        viewLayout2.setImage(UIImage(named: ""), for: .normal)
+        viewLayout3.setImage(UIImage(named: ""), for: .normal)
+    }
+
+    @IBAction func layout2(_ sender: Any) {
+        buttonImport1.isHidden = false
+        buttonImport2.isHidden = false
+        buttonImport3.isHidden = false
+        buttonImport4.isHidden = true
+        viewLayout1.setImage(UIImage(named: ""), for: .normal)
+        viewLayout2.setImage(UIImage(named: "Selected"), for: .normal)
+        viewLayout3.setImage(UIImage(named: ""), for: .normal)
+    }
+
+    @IBAction func layout3(_ sender: Any) {
+        buttonImport1.isHidden = false
+        buttonImport2.isHidden = false
+        buttonImport3.isHidden = false
+        buttonImport4.isHidden = false
+        viewLayout1.setImage(UIImage(named: ""), for: .normal)
+        viewLayout2.setImage(UIImage(named: ""), for: .normal)
+        viewLayout3.setImage(UIImage(named: "Selected"), for: .normal)
+    }
+
+    @IBAction func ButtonImportPhoto1(_ sender: Any) {
         addNewPicture()
+    }
+
+    @IBAction func swipeActionV2(_ sender: Any) {
+        self.swipeView.transform = CGAffineTransform(translationX: 0, y: -200)
+        self.sharePhoto()
+        self.swipeView.transform = .identity
+    }
+
+    func sharePhoto() {
+        let activityController =  UIActivityViewController(activityItems: [buttonImport1!], applicationActivities: nil)
+        present(activityController, animated: true, completion: nil)
     }
 
     func addNewPicture() {
         if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
-
             let myPickerController = UIImagePickerController()
             myPickerController.delegate = self
             myPickerController.sourceType = .photoLibrary
@@ -28,21 +77,16 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         }
     }
 
-     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-           // myImageView2.isHidden = true
-            // myImageView1.image = image
-
-            btn3.setImage(image, for: .normal)
-       }
+            buttonImport1.setImage(image, for: .normal)
+        }
         
         self.dismiss(animated: true, completion: nil)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // code
     }
 }
-
-
